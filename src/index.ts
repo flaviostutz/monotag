dd05a183e5592dfd4a4b6b1e5987fc390fcfadb3
flaviostutz <flaviostutz@gmail.com>
2025-02-05 11:56:21 +0100

    chore: exporting types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { nextTag } from './tag';
 export { releaseNotes } from './notes';
 export { lastTagForPrefix, filterCommits, summarizeCommits } from './git';
+export * from './types';
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export * from './BasicOptions';
+export * from './Commit';
+export * from './CommitsSummary';
+export * from './NextTagOptions';
+export * from './SemverLevel';
+export * from './TagNotes';
